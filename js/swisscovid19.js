73c21780a56206da3f472486b80d9accc9168848
Pfister <PFISTSA4@novartis.net>
2020-11-24 16:37:06 +0100

    update

--- js/swisscovid19.js
+++ js/swisscovid19.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-	// $('select[name=selValue]').val("CH");
-	// $('.selectpicker').selectpicker('refresh');
+	// var d1 = new Date();
+	// var d2 = new Date(20,11,20,19,59,0,0);
+	// console.log(d1)
+	// console.log(d2)
+	// console.log(getTime(d1-d2))
 	showCases();
-	// $(".btn.btn-primary.btn-sm.btn-canton").click();
-});
 
-// $(document).on('click', '.mdb-select.md-form.md-dark',function(){
-//     // $(".btn.btn-primary.btn-sm.btn-canton").focus();
-// });
+});
 
 $("select#canton").on("change", function(value) {
 	console.log($(this).val())
@@ -17,6 +16,22 @@ $("select#canton").on("change", function(value) {
 $(window).scroll(function(){
     $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 200);
   });
+
+// var getTime = function(timediff) {
+//     var days=Math.floor(numberOfHours/24);
+//     var remainder=numberOfHours % 24;
+//     var hours=Math.floor(remainder);
+//     var minutes=Math.floor(60*(remainder-hours));
+//     console.log(days)
+//     if(days>0)
+//     	return("Updated ",toString(days)," days ago")
+//     else if(hours>0)
+//     	return("Updated ",toString(hours)," hrs ago")
+//     else if(minutes>0)
+//     	return("Updated ",toString(minutes)," min ago")
+//     else
+//     	return("Updated few sec ago.")
+// }
 
 var showCases = function() {
 
@@ -35,10 +50,6 @@ var showCases = function() {
 	var cards = document.getElementsByClassName("jumbotron-cases");
 	for(var i = 0; i < cards.length; i++)
 		cards[i].style.display = "block";
-
-	// var cantn = document.getElementsByClassName("tr-canton");
-	// for(var i = 0; i < cantn.length; i++)
-	// 	cantn[i].style.display = "";
 
 	$(window).scrollTop(0);
 
@@ -63,10 +74,6 @@ var showTests = function() {
 	for(var i = 0; i < cards.length; i++)
 		cards[i].style.display = "block";
 
-	// var cantn = document.getElementsByClassName("tb-canton");
-	// for(var i = 0; i < cantn.length; i++)
-	// 	cantn[i].style.display = "";
-
 	$(window).scrollTop(0);
 
 	$(".btn.btn-primary.btn-sm.btn-tests").focus();
@@ -89,10 +96,6 @@ var showBeds = function() {
 	var cards = document.getElementsByClassName("jumbotron-beds");
 	for(var i = 0; i < cards.length; i++)
 		cards[i].style.display = "block";
-
-	// var cantn = document.getElementsByClassName("tr-canton");
-	// for(var i = 0; i < cantn.length; i++)
-	// 	cantn[i].style.display = "";
 
 	$(window).scrollTop(0);
 
@@ -117,10 +120,6 @@ var showICU = function() {
 	for(var i = 0; i < cards.length; i++)
 		cards[i].style.display = "block";
 
-	// var cantn = document.getElementsByClassName("tb-canton");
-	// for(var i = 0; i < cantn.length; i++)
-	// 	cantn[i].style.display = "";
-
 	$(window).scrollTop(0);
 
 	$(".btn.btn-primary.btn-sm.btn-icus").focus();
@@ -143,10 +142,6 @@ var showTrends = function() {
 	var cards = document.getElementsByClassName("jumbotron-trends");
 	for(var i = 0; i < cards.length; i++)
 		cards[i].style.display = "block";
-
-	// var cantn = document.getElementsByClassName("tr-canton");
-	// for(var i = 0; i < cantn.length; i++)
-	// 	cantn[i].style.display = "";
 
 	$(window).scrollTop(0);
 
